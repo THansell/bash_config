9f04363de22a4b4dd28d566bd2f776089aa826ba
Tim Hansell <thansell@nextgenfed.com>
2021-04-09 11:32:27 -0400

    Fixing centos_build.sh

--- bash_bin/centos_build.sh
+++ bash_bin/centos_build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(readlink -f $(dirname $0))
+
+source $SCRIPT_DIR/shinc/message.sh
+
 if [ -z $CENTOS_IMAGE ] ; then
-  CENTOS_IMAGE=centos_build:latest
+  echo_green "CENTOS_IMAGE variable not set."
+  echo_green "Using [docker-mti.di2e.net/maven:debian-3.6.3]"
+  CENTOS_IMAGE=docker-mti.di2e.net/maven:debian-3.6.3
+else
+  echo_green "CENTOS_IMAGE=[$CENTOS_IMAGE]"
 fi
 
 maven_build.sh $CENTOS_IMAGE
