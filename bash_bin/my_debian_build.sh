e8a0651814507f656eacab4301e9da18b1cae751
Tim Hansell <hansellt@MILO.localdomain>
2022-09-08 16:02:16 -0400

    Removing old stuff that is not needed

--- bash_bin/my_debian_build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo docker run -it --rm --name my-maven-project \
-                  -v $(wslpath -w $(realpath .)):/usr/src/mymaven:z \
-                  -v $(wslpath -w ${HOME}/.m2):/root/.m2:z \
-                  -w /usr/src/mymaven \
-                  --user=$(id -u):$(id -g)   \
-                  docker-mtt.di2e.net/maven:debian-3.6.3 \
-                mvn clean install
